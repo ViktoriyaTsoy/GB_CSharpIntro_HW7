d66ec966f0704aa660c6f427e3c76ebda8034e08
ViktoriyaTsoy <158477949+ViktoriyaTsoy@users.noreply.github.com>
2024-03-10 18:23:33 +0600

    added HW7

---
+++ Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Функция Аккермана
+int ReadInt(string msg)
+{
+    Console.WriteLine(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// Функция Аккермана
+
+int A(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+
+    if (n == 0)
+    {
+        return A(m - 1, 1);
+    }
+    else
+    {
+        return A(m - 1, A(m, (n - 1)));
+
+    }
+
+}
+
+int N = ReadInt("Enter N");
+int M = ReadInt("Enter M");
+Console.WriteLine(A(M,N));
---
+++ Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+
+int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+
+void PrintArray(int[] array, int index)
+{
+    if (index >= array.Length)
+    {
+        return;
+    }
+    else
+    {
+        PrintArray(array, index + 1);
+        Console.Write(array[index] + " ");
+    }
+}
+
+PrintArray(numbers,0);
